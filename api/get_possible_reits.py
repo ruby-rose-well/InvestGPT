63d8f95c0187c57858aa6a867f8bcb82f89811a9
ruby-rose-well <68598092+ruby-rose-well@users.noreply.github.com>
2023-04-16 01:16:56 +0800

    Add files via upload

--- api/get_possible_reits.py
+++ api/get_possible_reits.py
@@ -1,7 +1,18 @@
-def get_possible_reits(risk_value):
+import pandas as pd
+
+def get_possible_reits(profile):
     """
-    Use the risk value to select possible REITs from the database
-    :param risk_value: a scalar
+    Use the expected return risk value to select possible REITs from the database
+    :param profile: a scalar of expected return and risk
     :return: a list of REIT keys
     """
-    return ["EQIX", "SBAC", "GLPI", "LAMR"]
+
+
+    df = pd.read_excel('company.xls')
+    processed_company = df.loc[df["return"] >=profile[0]]
+    final_company = processed_company.loc[processed_company["SR"] <= profile[1]]
+    company_list = final_company.iloc[:, 1].tolist()
+    return company_list
+
+if __name__ == '__main__':
+    print(get_possible_reits([0.296,2.483]))
--- api/map_risk_profile.py
+++ api/map_risk_profile.py
@@ -1,5 +1,119 @@
-def map_risk_profile(profile):
+
+def init_map():
+    map = {"gender":0, "age":22, "income":50000, "personality":1, "deposit":900000,
+           "in_debt":0, "marital_status":2, "income_expectation":1000000, "investment_purpose":1, "favourable_plan":1}
+    return map
+
+
+
+
+def map_risk_profile(map):
     """
-    Map the risk profile (a dictionary) to a scalar value
+        Map the risk profile (a dictionary) to a scalar value
     """
-    return 0
+    expected_return = 1.0
+    risk = 1.0
+    #condition 10
+    if map["favourable_plan"] == 0:
+        expected_return = 0.08
+        risk = 1.0
+    elif map["favourable_plan"] == 1:
+        expected_return = 0.06
+        risk = 0.8
+    else:
+        expected_return = 0.03
+        risk = 0.05
+    #condition 1
+    if map["gender"] == 0:
+        expected_return *= 1.1
+        risk *= 1.1
+    else:
+        expected_return *= 0.9
+        risk *= 0.9
+    #condition 2
+    if 20 <= map["age"] <= 30:
+        expected_return *= 1.5
+        risk *= 1.5
+    elif 30 <= map["age"] <= 50:
+        expected_return *= 1.1
+        risk *= 1.0
+    else:
+        expected_return *= 0.8
+        risk *= 0.6
+    #condition 3
+    if 100000 <= map["income"] <= 300000:
+        expected_return *= 0.8
+        risk *= 0.8
+    elif 300000 <= map["income"] <= 500000:
+        expected_return *= 1.0
+        risk *= 1.0
+    else:
+        expected_return *= 1.2
+        risk *= 1.2
+    #condition 4
+    if map["personality"] == 0:
+        expected_return *= 0.9
+        risk *= 0.8
+    else:
+        expected_return *= 1.2
+        risk *= 1.1
+    #condition 5
+    if 100000 <= map["deposit"] <= 300000:
+        expected_return *= 0.8
+        risk *= 0.8
+    elif 300000 <= map["deposit"] <= 500000:
+        expected_return *= 1.0
+        risk *= 1.0
+    else:
+        expected_return *= 1.2
+        risk *= 1.2
+    #condition 6
+    if map["in_debt"] == 0:
+        expected_return *= 1.2
+        risk *= 0.9
+    else:
+        expected_return *= 0.9
+        risk *= 1.1
+    #condition 7
+    if map["marital_status"] == 0:
+        expected_return *= 1.2
+        risk *= 0.8
+    elif map["marital_status"] == 1:
+        expected_return *= 1.1
+        risk *= 0.9
+    elif map["marital_status"] == 2:
+        expected_return *= 1.0
+        risk *= 1.0
+    else:
+        expected_return *= 1.05
+        risk *= 0.85
+    #condition 8
+    if 100000 <= map["income_expectation"] <= 300000:
+        expected_return *= 0.8
+        risk *= 0.8
+    elif 300000 <= map["income_expectation"] <= 500000:
+        expected_return *= 1.0
+        risk *= 1.0
+    else:
+        expected_return *= 1.2
+        risk *= 1.2
+    #condition 9
+    if map["investment_purpose"] == 0:
+        expected_return *= 1.017
+        risk *= 0.8
+    elif map["investment_purpose"] == 1:
+        expected_return *= 1.2
+        risk *= 1.1
+    else:
+        expected_return *= 1.05
+        risk *= 0.7
+    result = [expected_return, risk]
+    # output：0.296,2.483
+    return result
+
+
+if __name__ == '__main__':
+    search = map_risk_profile(init_map())
+    for i in search:
+        print(i)
+
